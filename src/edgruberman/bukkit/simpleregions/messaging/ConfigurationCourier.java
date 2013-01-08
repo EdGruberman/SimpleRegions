2e2db648bcde302d481aa836195621673e95e039
EdGruberman <ed@rjump.com>
2013-01-08 01:33:29 -0700

    FIX: Updated messaging infrastructure

--- src/edgruberman/bukkit/simpleregions/messaging/ConfigurationCourier.java
+++ src/edgruberman/bukkit/simpleregions/messaging/ConfigurationCourier.java
@@ -1,10 +1,5 @@
 package edgruberman.bukkit.simpleregions.messaging;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.logging.Level;
-
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
@@ -15,11 +10,11 @@ import org.bukkit.plugin.Plugin;
  * uses message patterns stored in a {@link org.bukkit.configuration.ConfigurationSection ConfigurationSection}
  *
  * @author EdGruberman (ed@rjump.com)
- * @version 4.1.0
+ * @version 5.1.0
  */
 public class ConfigurationCourier extends Courier {
 
-    /** section containing message patterns; null if basePath should be used */
+    /** section containing message patterns */
     protected final ConfigurationSection base;
 
     protected ConfigurationCourier(final ConfigurationCourier.Factory parameters) {
@@ -32,27 +27,21 @@ public class ConfigurationCourier extends Courier {
         return this.base;
     }
 
-    /** draft Messages (single for string value, multiple for string list value) */
-    public List<Message> compose(final String key, final Object... arguments) {
-        final List<Message> messages = new ArrayList<Message>();
-
-        if (this.base.isString(key)) {
-            messages.add(this.draft(this.base.getString(key), arguments));
-            return messages;
-        }
-
-        if (!this.base.isList(key)) {
-            this.plugin.getLogger().log(Level.FINEST, "Unusable Message pattern \"{1}\" at \"{0}\" key", new Object[] { this.base.get(key), key });
-            return Collections.emptyList();
-        }
-
-        for (final String pattern : this.base.getStringList(key)) messages.add(this.draft(pattern, arguments));
-        return messages;
+    /** @return String supplied in base configuration at key path; null if not a String */
+    public String pattern(final String key) {
+        if (!this.base.isString(key)) return null;
+        return this.base.getString(key);
     }
 
-    /** deliver messages to recipients and record log entry for each message (this will not timestamp the message) */
-    public void submit(final Recipients recipients, final List<Message> messages) {
-        for (final Message message : messages) this.submit(recipients, message);
+    /**
+     * preliminary Message construction before formatting for target recipient (timestamp argument prepended if configured)
+     *
+     * @param key path to message text that can contain format elements in base configuration
+     */
+    public Message compose(final String key, final Object... arguments) {
+        final String pattern = this.pattern(key);
+        if (pattern == null) return null;
+        return this.draft(pattern, arguments);
     }
 
     /**
@@ -62,38 +51,40 @@ public class ConfigurationCourier extends Courier {
      */
     @Override
     public String format(final String key, final Object... arguments) {
-        return super.format(this.getBase().getString(key), arguments);
+        final String pattern = this.pattern(key);
+        if (pattern == null) return null;
+        return super.format(pattern, arguments);
     }
 
-    @Override
     public void send(final CommandSender target, final String key, final Object... arguments) {
-        final Recipients recipients = new Individual(target);
-        final List<Message> messages = this.compose(key, arguments);
-        this.submit(recipients, messages);
+        final String pattern = this.pattern(key);
+        if (pattern == null) return;
+        this.sendMessage(target, pattern, arguments);
     }
 
-    @Override
     public void broadcast(final String key, final Object... arguments) {
-        final Recipients recipients = new ServerPlayers();
-        final List<Message> messages = this.compose(key, arguments);
-        this.submit(recipients, messages);
+        final String pattern = this.pattern(key);
+        if (pattern == null) return;
+        this.broadcastMessage(pattern, arguments);
     }
 
-    @Override
     public void world(final World target, final String key, final Object... arguments) {
-        final Recipients recipients = new WorldPlayers(target);
-        final List<Message> messages = this.compose(key, arguments);
-        this.submit(recipients, messages);
+        final String pattern = this.pattern(key);
+        if (pattern == null) return;
+        this.worldMessage(target, pattern, arguments);
     }
 
-    @Override
     public void publish(final String permission, final String key, final Object... arguments) {
-        final Recipients recipients = new PermissionSubscribers(permission);
-        final List<Message> messages = this.compose(key, arguments);
-        this.submit(recipients, messages);
+        final String pattern = this.pattern(key);
+        if (pattern == null) return;
+        this.publishMessage(permission, pattern, arguments);
     }
 
 
+
+    public static Factory create(final Plugin plugin) {
+        return Factory.create(plugin);
+    }
 
     public static class Factory extends Courier.Factory {
 
@@ -111,19 +102,24 @@ public class ConfigurationCourier extends Courier {
 
         /** @param section base section containing message patterns */
         public Factory setBase(final ConfigurationSection section) {
+            if (section == null) throw new IllegalArgumentException("ConfigurationSection can not be null");
             this.base = section;
             return this;
         }
 
         /** @param path path to section relative to current base section containing message patterns */
         public Factory setPath(final String path) {
-            this.base = this.base.getConfigurationSection(path);
+            final ConfigurationSection section = this.base.getConfigurationSection(path);
+            if (section == null) throw new IllegalArgumentException("ConfigurationSection not found: " + path);
+            this.setBase(section);
             return this;
         }
 
         /** @param key path to color code prefix character in base configuration */
         public Factory setColorCode(final String key) {
-            this.setColorCode(this.base.getString(key).charAt(0));
+            final String value = this.base.getString(key);
+            if (value == null) throw new IllegalArgumentException("Color code not found: " + this.base.getCurrentPath() + this.base.getRoot().options().pathSeparator() + key);
+            this.setColorCode(value.charAt(0));
             return this;
         }
 
